b5f9db80630cad1ebc2c02bb5440d839f3d9cf08
Taha Mehjabin <marvime660@gmail.com>
2025-02-08 14:00:38 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$user = "root";
+$host = "";
+$user = "";
 $password = "";
 $dbname = "women_safety";
-$conn = mysqli_connect("localhost","root","","women_safety");
+$conn = mysqli_connect("","","","");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
